6d6a1bcc194411e65fbca62f2d68b7fe4a51befa
marliseu <marlise.au@gmail.com>
2016-02-22 16:51:20 -0500

    Separated toggle function

--- app/Card.js
+++ app/Card.js
@@ -4,14 +4,35 @@ import CheckList from './CheckList';
 
 
 class Card extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false
+    };
+  }
+
+  toggleDetails() {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
   render() {
-    return (
-      <div className="card">
-        <div className="card_title">{this.props.title}</div>
+    //If showDetails is true, render variable cardDetails containing the card_details div
+    let cardDetails;
+    if(this.state.showDetails){
+      cardDetails = (
         <div className="card_details">
           {this.props.description}
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
+      );
+    };
+
+    return (
+      <div className="card">
+        <div className="card_title" onClick={this.toggleDetails.bind(this)}>
+          {this.props.title}
+        </div>
+        {cardDetails}
       </div>
     );
   }
