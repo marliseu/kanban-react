2c374fb33ef5a0634e4a0d466c30a30898fc0b2e
marliseu <marlise.au@gmail.com>
2016-02-23 12:34:12 -0500

    Added color coded div to cards

--- app/Card.js
+++ app/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 
 import CheckList from './CheckList';
 
@@ -21,14 +22,25 @@ class Card extends Component {
     if(this.state.showDetails){
       cardDetails = (
         <div className="card_details">
-          {this.props.description}
+          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       );
     };
 
+    let sideColor = {
+      position: 'absolute',
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7,
+      backgroundColor: this.props.color
+    };
+
     return (
       <div className="card">
+        <div style={sideColor} />
         <div className={this.state.showDetails? "card_title card_title--is-open" : "card_title"
             } onClick={this.toggleDetails.bind(this)}>
           {this.props.title}
--- app/CheckList.js
+++ app/CheckList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 
 
 class CheckList extends Component {
