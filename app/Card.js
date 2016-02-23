db3daaa2a59204e4a59b34a2b50bd576103dcd3a
marliseu <marlise.au@gmail.com>
2016-02-22 19:21:39 -0500

    Added ternary operator to change arrow direction on expand

--- app/Card.js
+++ app/Card.js
@@ -29,7 +29,8 @@ class Card extends Component {
 
     return (
       <div className="card">
-        <div className="card_title" onClick={this.toggleDetails.bind(this)}>
+        <div className={this.state.showDetails? "card_title card_title--is-open" : "card_title"
+            } onClick={this.toggleDetails.bind(this)}>
           {this.props.title}
         </div>
         {cardDetails}
